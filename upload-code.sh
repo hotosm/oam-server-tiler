c5438fd3931b1d3f2709a4713fa18e316b9ca23c
Seth Fitzsimmons <seth@mojodna.net>
2015-10-19 14:06:12 -0700

    Version deployed code

--- upload-code.sh
+++ upload-code.sh
@@ -1,8 +1,6 @@
 TARGET=s3://oam-server-tiler/emr
 VERSION=0.1.0
 
-cd mosaic
-./sbt assembly
-cd ..
-aws s3 cp chunk/chunk.py $TARGET/chunk.py
-aws s3 cp mosaic/target/scala-2.10/oam-tiler-assembly-$VERSION.jar $TARGET/mosaic.jar
+cd mosaic && ./sbt assembly
+aws s3 cp chunk/chunk.py $TARGET/chunk-${VERSION}.py
+aws s3 cp mosaic/target/scala-2.10/oam-tiler-assembly-$VERSION.jar $TARGET/mosaic-${VERSION}.jar
