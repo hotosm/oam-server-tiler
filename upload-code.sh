122ccac75044b5ab1c6a993f1251ff0a3ed341fa
Seth Fitzsimmons <seth@mojodna.net>
2015-10-19 14:09:01 -0700

    Publish EMR configuration

--- upload-code.sh
+++ upload-code.sh
@@ -4,3 +4,4 @@ VERSION=0.1.0
 cd mosaic && ./sbt assembly
 aws s3 cp chunk/chunk.py $TARGET/chunk-${VERSION}.py
 aws s3 cp mosaic/target/scala-2.10/oam-tiler-assembly-$VERSION.jar $TARGET/mosaic-${VERSION}.jar
+aws s3 cp emr.json $TARGET/configurations-${VERSION}.json
