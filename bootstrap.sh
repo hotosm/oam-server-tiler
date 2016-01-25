c5a1a2441f2c9caea1aecff90a66c8f9fe3597af
Seth Fitzsimmons <seth@mojodna.net>
2016-01-25 14:25:10 -0800

    inotify-tools is unnecessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,3 @@ sudo ln -s /usr/lib64/libproj.so.0 /usr/lib64/libproj.so
 aws s3 cp s3://oam-server-tiler/emr/gdal-1.11.2-amz2.tar.gz - | sudo tar zxf - -C /usr/local
 aws s3 cp s3://oam-server-tiler/emr/gdal-1.11.2-amz2-py.tar.gz - | sudo tar zxf - -C /usr/local/lib64/python2.6/site-packages
 sudo GDAL_CONFIG=/usr/local/bin/gdal-config pip-2.7 install boto3 rasterio==0.29.0 mercantile psutil
-
-sudo yum -y install inotify-tools
